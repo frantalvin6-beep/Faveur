59b3d6754169716a3980c2faa72ecd9a008fca69
Frantalvin <frantalvin6@gmail.com>
2025-08-05 15:01:54 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -3,8 +3,10 @@
 import * as React from "react"
 import { GripVertical } from "lucide-react"
 import {
-  ResizablePanelGroup as ResizablePanelGroupPrimitive,
-  type ResizablePanelGroupProps,
+  PanelGroup as ResizablePanelGroupPrimitive,
+  type PanelGroupProps,
+  Panel as ResizablePanel,
+  PanelResizeHandle as ResizableHandlePrimitive,
 } from "react-resizable-panels"
 
 import { cn } from "@/lib/utils"
@@ -12,7 +14,7 @@ import { cn } from "@/lib/utils"
 const ResizablePanelGroup = ({
   className,
   ...props
-}: ResizablePanelGroupProps) => (
+}: PanelGroupProps) => (
   <ResizablePanelGroupPrimitive
     className={cn(
       "flex h-full w-full data-[panel-group-direction=vertical]:flex-col",
@@ -22,18 +24,16 @@ const ResizablePanelGroup = ({
   />
 )
 
-const ResizablePanel = ResizablePanelGroupPrimitive.Panel
-
 const ResizableHandle = ({
   withHandle,
   className,
   ...props
 }: React.ComponentProps<
-  typeof ResizablePanelGroupPrimitive.ResizeHandle
+  typeof ResizableHandlePrimitive
 > & {
   withHandle?: boolean
 }) => (
-  <ResizablePanelGroupPrimitive.ResizeHandle
+  <ResizableHandlePrimitive
     className={cn(
       "relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 [&[data-panel-group-direction=vertical]>div]:rotate-90",
       className
@@ -45,7 +45,7 @@ const ResizableHandle = ({
         <GripVertical className="h-2.5 w-2.5" />
       </div>
     )}
-  </ResizablePanelGroupPrimitive.ResizeHandle>
+  </ResizableHandlePrimitive>
 )
 
 export { ResizablePanelGroup, ResizablePanel, ResizableHandle }
