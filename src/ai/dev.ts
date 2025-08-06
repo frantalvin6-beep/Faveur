adfa5a78f4dc6abab32afe74fd860a2a538e65eb
Frantalvin <frantalvin6@gmail.com>
2025-08-06 16:53:06 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/ai/dev.ts
+++
@@ -1,7 +0,0 @@
-import { config } from 'dotenv';
-config();
-
-// Assurez-vous que tous les flux sont importés ici
-import '@/ai/flows/generate-ai-powered-reports';
-import '@/ai/flows/calculate-student-gpa';
-import '@/ai/flows/ai-assistant';
