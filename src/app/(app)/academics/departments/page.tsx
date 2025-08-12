528f245834343d526634a422f5a1c30eeda38942
Frantalvin <frantalvin6@gmail.com>
2025-08-12 10:10:36 +0000

    Refais les modifications pour cette page

--- src/app/(app)/academics/departments/page.tsx
+++ src/app/(app)/academics/departments/page.tsx
@@ -2,12 +2,7 @@
 'use client';
 
 import * as React from 'react';
-import { getDepartments, addDepartment, deleteDepartment, Department } from '@/lib/data';
 import { DepartmentsTable } from '@/components/academics/departments-table';
-import { useToast } from '@/hooks/use-toast';
-import { Skeleton } from '@/components/ui/skeleton';
-
-export const dynamic = 'force-dynamic';
 
 export default function DepartmentsPage() {
     return (
