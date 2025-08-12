0105ac78bf5e113f23670a73d0349b6ae2b000e3
Frantalvin <frantalvin6@gmail.com>
2025-08-12 13:15:24 +0000

    C'est bon mais la largeur n'est pas bonne

--- src/app/(app)/academics/calendar/page.tsx
+++ src/app/(app)/academics/calendar/page.tsx
@@ -1,8 +1,9 @@
+
 import { AcademicCalendar } from '@/components/academics/academic-calendar';
 
 export default function CalendarPage() {
   return (
-    <div>
+    <div className="w-full">
       <h1 className="text-3xl font-bold mb-4">Calendrier académique</h1>
       <AcademicCalendar />
     </div>
